d3f34fc400e8cb18304a2dc0bc47210c348ebd9f
Роман <gerasimov.roman.v@mail.ru>
2019-12-04 20:45:04 +0300

    feat: add nomalize, gtid, variables, iterface, header styles in template folder

--- gulpfile.js
+++ gulpfile.js
@@ -14,13 +14,13 @@ const sass = require('gulp-sass');
   watch("./*.html").on('change', browserSync.reload);
   watch("./sass/**/*.sass", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
-};
+}
 
  function serveSass() {
   return src("./sass/*.sass")
       .pipe(sass())
       .pipe(dest("./css"))
       .pipe(browserSync.stream());
-};
+}
 
 exports.serve = bs;
