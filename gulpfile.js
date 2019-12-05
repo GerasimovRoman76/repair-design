f2ed32d3bd5b45deda1f68556530065ba4817b79
Роман <gerasimov.roman.v@mail.ru>
2019-12-05 22:06:12 +0300

    add: instagram button

--- gulpfile.js
+++ gulpfile.js
@@ -4,24 +4,24 @@ const sass = require('gulp-sass');
 
 
 // Static server
- function bs() {
-  serveSass();
-  browserSync.init({
-      server: {
-          baseDir: "./"
-      }
-  });
-  watch("./*.html").on('change', browserSync.reload);
-     watch("./sass/**/*.sass", serveSass);
-     watch("./sass/**/*.scss", serveSass);
-  watch("./js/*.js").on('change', browserSync.reload);
+function bs() {
+    serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./sass/**/*.scss", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
 }
 
- function serveSass() {
-  return src("./sass/**/.sass", "./sass/**/.scss")
-      .pipe(sass())
-      .pipe(dest("./css"))
-      .pipe(browserSync.stream());
+function serveSass() {
+    return src("./sass/**/.sass", "./sass/**/.scss")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
 }
 
 exports.serve = bs;
