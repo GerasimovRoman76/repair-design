8744cc34df45f7b9bd4775a782e95f2ed26f3ae1
Роман <gerasimov.roman.v@mail.ru>
2019-12-17 01:30:39 +0300

    feat: add  section projects

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 });
 */
+/*выше код js для модального окна*/
 
 $(document).ready(function () {
   var modal = $('.modal'),
@@ -26,4 +27,23 @@ $(document).ready(function () {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() + 10)
+  bullets.css('left', prev.width() + 10)
 });
